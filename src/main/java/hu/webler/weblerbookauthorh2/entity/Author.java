a26ee485a308fe6411ca976e5ad9d3a653f97396
Szőke Roland <szokeroland0@gmail.com>
2024-02-03 14:31:11 +0100

    create base models Author and Book

---
+++ src/main/java/hu/webler/weblerbookauthorh2/entity/Author.java
@@ -0,0 +1,51 @@
+package hu.webler.weblerbookauthorh2.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+    public Author() {
+    }
+
+    public Author(String name, List<Book> books) {
+        this.name = name;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
